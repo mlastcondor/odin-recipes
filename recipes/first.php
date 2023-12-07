095dbd999ba2530ea9f52ad94ce16f608854fe4d
mlastcondor <mlastcondor@googlemail.com>
2023-12-07 00:00:11 +0000

    Add PHP Page for testing

---
+++ recipes/first.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>First PHP</title>
+    </head>
+    <body>
+        <!-- Script 1.3 - first.php -->
+        <p>This is standatd HTML.</p>
+    <?php
+    echo "This was generated using PHP!";
+    ?>
+    </body>
+</html>
+
